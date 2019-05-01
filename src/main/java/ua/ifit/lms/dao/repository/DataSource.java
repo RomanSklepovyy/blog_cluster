550fc85c119366a9111b3eab5cc0f81182bdc7ef
RomanSklepovyy <romansklepovyy1@gmail.com>
2019-05-01 23:21:23 +0300

    DataSource

--- src/main/java/ua/ifit/lms/dao/repository/DataSource.java
+++ src/main/java/ua/ifit/lms/dao/repository/DataSource.java
@@ -1,4 +1,43 @@
 package ua.ifit.lms.dao.repository;
 
-public class DataSource {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataSource implements AutoCloseable {
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/data?user=root?password=1111";
+
+    private Connection connection = null;
+
+    public DataSource () {
+        try {
+            Class.forName(JDBC_DRIVER).newInstance();
+        } catch (IllegalAccessException | InstantiationException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null) {
+                connection = DriverManager.getConnection(DB_URL);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error (getConnection)" + e.toString());
+        }
+        return connection;
+    }
+
+    @Override
+    public void close() throws Exception {
+        try {
+            if (connection!=null)
+                connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
