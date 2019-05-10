7af725546038d1e8530411441d6e1c0d81c0a1b7
Yurii Huley <vasilvasilovichvasil@gmail.com>
2019-05-10 16:58:23 +0300

    sql.sql user aded

--- sql.sql
+++ sql.sql
@@ -12,5 +12,16 @@ create table note
 INSERT INTO data.note (id, user_id, title, text, date_created, date_last_edited)
 VALUES (1, 1, 'Hello', 'Hello world!', '2019-04-19 16:04:26', '2019-04-19 16:04:31');
 
+create table user
+(
+	id int auto_increment
+		primary key,
+	email varchar(50) not null,
+	password varchar(20) null,
+	name varchar(30) null,
+	date_created datetime not null,
+	date_last_entered datetime not null
+);
+
 
 
